2cb2e1f4756a956fa99bc6594309bb0a8e8359bb
Silent <silent3_3@mail.ru>
2023-04-25 17:45:13 +0300

    Решение здачи №66

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите первое значение: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите последнее значение: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
